e055bb129d9113b1428ec9716a77e648da37d03a
bcp-nadeem <nadeem.qureshi@bimcap.com>
2024-11-14 16:36:58 +0530

    New Style

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { AuthProvider } from './store/AuthContext.jsx';
 
 function App() {
   return (
-    //browser router
+    //browser router//
     <AuthProvider>
     <RouterProvider router={routes} />
     </AuthProvider>
